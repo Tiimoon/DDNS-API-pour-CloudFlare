52da3e8baea66772654774020c9573c08f1eb82f
Thomas Rbt <33023463+Tiimoon@users.noreply.github.com>
2021-11-06 05:11:49 +0100

    Update DDNS.sh

--- DDNS.sh
+++ DDNS.sh
@@ -2,33 +2,21 @@
 
 echo "Bienvenue dans le script DDNS pour CloudFlare !"
 read -p "Voulez-vous lancer le script de configuration ? Y/N :" accept
-
 if [ "$accept" = "y" ]
 then
     sudo apt update
     sudo apt -y upgrade
     sudo apt install ddclient libdata-validate-ip-perl
-
     sudo dpkg-reconfigure ddclient
-
-
     wget https://sourceforge.net/projects/ddclient/files/ddclient/ddclient-3.9.0/ddclient-3.9.0.tar.gz
     tar -xvf ddclient-3.9.0.tar.gz
-
-
-
     sudo cp -f ddclient-3.9.0/ddclient /usr/sbin/ddclient
-
     sudo mkdir /etc/ddclient
     sudo mv /etc/ddclient.conf /etc/ddclient
-
     rm ddclient-3.9.0.tar.gz
     rm -R ddclient-3.9.0
-
     cd /etc/ddclient/
-    
     sudo rm ./ddclient.conf
-
     read -p " Saisir le temp en seconde de mise à jour de l'ip ex : 300 (pour 5mins) " Temp
     read -p " Saisir l'email lié au compte CloudFlare " Email
     read -p " Saisir la clé API globale de votre compte CloudFlare " CleApi
@@ -42,11 +30,8 @@ then
     echo "password=$CleApi" >> ddclient.conf 
     echo "zone=$Domaine" >> ddclient.conf 
     echo "$SubDomaine" >> ddclient.conf 
-
     sudo ddclient -query
-
     echo "Félicitation votre server mettra à jour votre DNS en fonction de votre IP dynamique toute les $Temp secondes"
-
 else
         echo "ok une prochaine fois !"
 fi
